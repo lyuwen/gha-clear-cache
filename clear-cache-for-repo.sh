0961f3f876ef345e6ad199037d85f164ae30f3dd
Lyuwen Fu <fulvwen@gmail.com>
2023-09-23 20:32:07 +0800

    update clear script

--- clear-cache-for-repo.sh
+++ clear-cache-for-repo.sh
@@ -2,7 +2,7 @@ set -e
 for repo in $@;
 do
   echo Deleting caches for $repo;
-  cacheKeylist=$(gh actions-cache list -R $repo -L 100 | grep "day ago" | cut -f 1)
+  cacheKeylist=$(gh actions-cache list -R $repo -L 100 | cut -f 1)
   for key in $cacheKeylist;
   do
     gh actions-cache delete $key -R $repo --confirm;
